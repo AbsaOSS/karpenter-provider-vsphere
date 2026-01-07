806c3bb890982cadb4a656e88279a35ada3c58df
Ndomi-Nqeketo <ndomi.nqeketo@absa.africa>
2026-01-07 14:10:58 +0200

    K8s Version test
    Signed-off-by: Ndomi-Nqeketo <ndomi.nqeketo@absa.africa>

--- go.mod
+++ go.mod
@@ -6,12 +6,14 @@ require (
 	github.com/awslabs/operatorpkg v0.0.0-20250624064700-e9977193119b
 	github.com/blang/semver/v4 v4.0.0
 	github.com/go-logr/zapr v1.3.0
+	github.com/google/gnostic-models v0.7.0
 	github.com/mitchellh/hashstructure/v2 v2.0.2
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.9.1
 	github.com/samber/lo v1.51.0
 	github.com/stretchr/testify v1.11.1
 	github.com/vmware/govmomi v0.52.0
+	go.uber.org/mock v0.6.0
 	go.uber.org/multierr v1.11.0
 	k8s.io/api v0.34.1
 	k8s.io/apiextensions-apiserver v0.34.1
@@ -36,7 +38,6 @@ require (
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/google/btree v1.1.3 // indirect
-	github.com/google/gnostic-models v0.7.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
---
+++ pkg/providers/kubernetesversion/kubernetesversion_test.go
@@ -0,0 +1,113 @@
+package kubernetesversion
+
+import (
+	"context"
+
+	"github.com/absaoss/karpenter-provider-vsphere/pkg/mocks"
+	"github.com/patrickmn/go-cache"
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/mock/gomock"
+	"k8s.io/apimachinery/pkg/version"
+
+	"testing"
+	"time"
+)
+
+func TestKubeServerVersion(t *testing.T) {
+	// arrange
+	controller := gomock.NewController(t) // initialize mock controller
+	defer controller.Finish()             // ensure resources are cleaned up
+
+	v := version.Info{GitVersion: "v1.2.3+rke2r1"}
+	q := mocks.NewMockInterface(controller)
+	d := mocks.NewMockDiscoveryInterface(controller)
+	d.EXPECT().ServerVersion().Return(&v, nil).AnyTimes()
+
+	q.EXPECT().Discovery().Return(d).AnyTimes()
+
+	c := cache.New(time.Second*10, time.Second*2)
+	provider := NewKubernetesVersionProvider(q, c)
+	// act
+	str, err := provider.KubeServerVersion(context.TODO())
+
+	// assert
+	assert.NoError(t, err)
+	assert.Equal(t, "1.2.3", str)
+}
+
+func TestKubeServerVersions(t *testing.T) {
+	var tests = []struct {
+		name            string
+		expectedError   bool
+		version         *version.Info
+		expectedVersion string
+		discoveryError  error
+	}{
+		{
+			name:            "valid version",
+			expectedError:   false,
+			version:         &version.Info{GitVersion: "v1.20.4+rke2r1"},
+			expectedVersion: "1.20.4",
+		},
+		{
+			name:            "valid version without prefix",
+			expectedError:   false,
+			version:         &version.Info{GitVersion: "1.21.0"},
+			expectedVersion: "1.21.0",
+		},
+		{
+			name:            "valid version with different suffix",
+			expectedError:   false,
+			version:         &version.Info{GitVersion: "v1.22.1+customsuffix"},
+			expectedVersion: "1.22.1+customsuffix",
+		},
+		{
+			name:            "empty version",
+			expectedError:   false,
+			version:         &version.Info{GitVersion: ""},
+			expectedVersion: "",
+		},
+		{
+			name:           "discovery error",
+			expectedError:  true,
+			version:        nil,
+			discoveryError: assert.AnError,
+		},
+		{
+			name:            "funny version",
+			expectedError:   false,
+			version:         &version.Info{GitVersion: "!!@@##???"},
+			expectedVersion: "!!@@##???",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// arrange
+			controller := gomock.NewController(t) // initialize mock controller
+			defer controller.Finish()             // ensure resources are cleaned up
+
+			q := mocks.NewMockInterface(controller)
+			d := mocks.NewMockDiscoveryInterface(controller)
+			d.EXPECT().ServerVersion().Return(test.version, test.discoveryError).AnyTimes()
+
+			q.EXPECT().Discovery().Return(d).AnyTimes()
+
+			c := cache.New(time.Second*10, time.Second*2)
+			provider := NewKubernetesVersionProvider(q, c)
+
+			// act
+			str, err := provider.KubeServerVersion(context.TODO())
+
+			// assert
+			if test.expectedError {
+				assert.Error(t, err)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.Equal(t, test.expectedVersion, str)
+
+		})
+	}
+}
